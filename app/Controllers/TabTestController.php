53947d164738671e3c4c665d7a9fc7888666a510
ahmetcanerdem <acanerdem99@gmail.com>
2022-05-18 10:58:08 +0300

    TabTest Extension typo fixed

--- app/Controllers/TabTestController.php
+++ app/Controllers/TabTestController.php
@@ -3,13 +3,13 @@ namespace App\Controllers;
 
 class TabTestController
 {
-    
+
     function tab1(){
-        return respond("Merhaba Dünya !!",200);
+        return respond("Merhaba Dunyali!!",200);
     }
 
     function tab2(){
-        return respond("Merhaba Uzaylı !!",200);
+        return respond("Merhaba Uzayli!!",200);
     }
 }
 ?>
